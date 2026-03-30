29489ee559b9aea2c77f58cba8884c6f432aefd6
Meet Dharmesh Gandhi <meetgandhi13062005@gmail.com>
2026-03-30 13:38:23 +0530

    style: made the loader look better

--- frontend/components/loader/Loader.tsx
+++ frontend/components/loader/Loader.tsx
@@ -1,22 +1,5 @@
-import { CSSProperties } from "react";
 import "./loader.css";
 
-export default function Loader({ len = 3 }: { len?: number }) {
-	return (
-		<div className="flex">
-			{Array(len)
-				.fill(0)
-				.map((_, ind) => (
-					<div
-						key={`loader-{${ind}}`}
-						className="loader"
-						style={
-							{
-								"--n": ind,
-							} as CSSProperties
-						}
-					></div>
-				))}
-		</div>
-	);
+export default function Loader() {
+	return <div className="loader-1"></div>;
 }
